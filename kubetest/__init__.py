6996cb9b9b9a25dad8bfa997f7af5f25584ca190
Terry <terry@3si.me>
2022-07-26 21:40:26 +0200

    Rename

--- kubetest/__init__.py
+++ kubetest/__init__.py
@@ -1,9 +1,9 @@
-"""kubetest -- a Kubernetes integration test framework in Python."""
+"""k8st3sT -- a Kubernetes integration test framework in Python."""
 
-__title__ = "kubetest"
+__title__ = "k8st3sT"
 __version__ = "0.9.5"
 __description__ = "A Kubernetes integration test framework in Python."
 __author__ = "Vapor IO"
 __author_email__ = "vapor@vapor.io"
-__url__ = "https://github.com/vapor-ware/kubetest"
+__url__ = "https://github.com/3sIT/k8st3sT"
 __license__ = "GNU General Public License v3.0"
