c78065f7fb3683326d9fabfad65224d5096e5624
Terry <terry@3si.me>
2022-08-10 23:02:22 +0200

    Update Python compatibility list

--- setup.py
+++ setup.py
@@ -46,7 +46,9 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     # this make a plugin available to pytest
     # https://docs.pytest.org/en/latest/writing_plugins.html#making-your-plugin-installable-by-others
